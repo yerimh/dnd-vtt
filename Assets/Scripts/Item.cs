b91b3ada33e9203fb719f2413fdbb45ddc8a411e
Yerim Heo <yerimh@uw.edu>
2020-12-01 01:58:06 -0800

    now you can delete items/spells/notes

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] TMP_InputField itemNameInput;
+    private string itemName;
+    private int quantity;
+    private bool isMagical;
+    private string notes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        itemName = itemNameInput.text;
+    }
+
+    public Item(string item) {
+        itemName = item;
+    }
+}
--- Assets/Scripts/ProfileManager.cs
+++ Assets/Scripts/ProfileManager.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
+/**
+ * TODO: Item class is monobehaviour. Either attach it to each clone, or have it be its own thing for data collection
+ */
 public class ProfileManager : MonoBehaviour
 {
-    [SerializeField] GameObject item;
+    [SerializeField] GameObject itemContainer;
     [SerializeField] GameObject inventoryPanel, spellsPanel, notesPanel;
+    private List<Item> inventory = new List<Item>();
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +26,15 @@ public class ProfileManager : MonoBehaviour
     }
 
     public void AddItem(GameObject panel) {
-        Instantiate(item, panel.transform);
+        GameObject newItemContainer = Instantiate(itemContainer, panel.transform);
+        Button delete = newItemContainer.GetComponentInChildren<Button>();
+        delete.onClick.AddListener(DeleteItem);
+
+        // inventory.Add(new Item(itemContainer.GetComponentInChildren<TMP_InputField>().text));
     }
 
-    public void DeleteItem(GameObject btn) {
-        Debug.Log(btn.transform.parent.gameObject);
+    public void DeleteItem() {
+        GameObject deleting = EventSystem.current.currentSelectedGameObject;
+        Destroy(deleting.transform.parent.gameObject);
     }
 }
